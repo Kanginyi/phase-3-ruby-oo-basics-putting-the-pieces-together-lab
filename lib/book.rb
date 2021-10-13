f8891b40e1ba136764cc4a600b85f01dc03ed3be
Eric Yi <erickanginyi@gmail.com>
2021-10-13 14:37:58 -0400

    commit tree 0d04f0c299b7790930c2d6a05d54b50eacb00562

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    # initialize title
+    def initialize(title)
+        @title = title
+    end
 
+    attr_accessor :title, :author, :page_count, :genre
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
